6e39a307d8bc6c8dc4e19a9030228044d0f026cd
Carlos Natan <cnatan@stone.com.br>
2017-02-22 11:39:25 -0300

    First functional version with Parameters.xml

---
+++ WhoAmIWebApplication/Controllers/SettingsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Web.Http;
+
+namespace WhoAmIWebApplication.Controllers
+{
+    public class SettingsController : ApiController
+    {
+
+        [HttpGet]
+        public string GetSettings()
+        {
+            string value = ConfigurationManager.AppSettings["MySetting"];
+            return value;
+        }
+    }
+}
---
+++ WhoAmIWebApplication/Controllers/WhoAmIController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Web.Http;
+
+namespace WhoAmIWebApplication.Controllers
+{
+    public class WhoAmIController : ApiController
+    {
+        [HttpGet]
+        public string Get()
+        {
+            string response = $"{Environment.MachineName} - v4";
+            return response;
+        }
+    }
+}
